f447955133bf0dcf84777c69cb2f10f0be4bc926
HANO Hiroyuki <onahdwahan@gmail.com>
2025-12-13 09:12:13 +0900

    Refactor StdioTransport to remove generic parameters and simplify implementation

--- src/lsp/stdio_transport.rs
+++ src/lsp/stdio_transport.rs
@@ -1,26 +1,16 @@
 // low-level stdio transport: framing (Content-Length) and raw read/write
 use crate::lsp::transport::LspTransport;
 use anyhow::anyhow;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::io::{AsyncReadExt, AsyncWriteExt, BufReader};
+use tokio::process::{ChildStdin, ChildStdout};
 
-/*
 pub struct StdioTransport {
     writer: ChildStdin,
     reader: BufReader<ChildStdout>,
 }
-*/
-
-pub struct StdioTransport<W, R> {
-    writer: W,
-    reader: R,
-}
 
 #[async_trait::async_trait]
-impl<W, R> LspTransport for StdioTransport<W, R>
-where
-    W: AsyncWriteExt + Unpin + Send + Sync + 'static,
-    R: AsyncReadExt + Unpin + Send + Sync + 'static,
-{
+impl LspTransport for StdioTransport {
     async fn write(&mut self, json_body: &str) -> anyhow::Result<()> {
         let length = json_body.len();
         let header = format!("Content-Length: {}\r\n\r\n", length);
@@ -51,12 +41,8 @@ where
     }
 }
 
-impl<W, R> StdioTransport<W, R>
-where
-    W: AsyncWriteExt + Unpin + Send + Sync + 'static,
-    R: AsyncReadExt + Unpin + Send + Sync + 'static,
-{
-    pub fn new(writer: W, reader: R) -> StdioTransport<W, R> {
+impl StdioTransport {
+    pub fn new(writer: ChildStdin, reader: BufReader<ChildStdout>) -> StdioTransport {
         StdioTransport { writer, reader }
     }
 }
